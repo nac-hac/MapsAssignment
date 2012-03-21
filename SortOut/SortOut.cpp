15f6e68b6c7e77de8acb43cd4f6343e8303cf8b7
Nico <nicohornung@web.de>
2012-03-21 14:40:45 +0000

    added function prototypes and timers

--- SortOut/SortOut.cpp
+++ SortOut/SortOut.cpp
@@ -22,32 +22,45 @@ using namespace std;
 
 int data[MAX_ROWS][MAX_COLS];
 
-CStopWatch s1,s2,s3;
+CStopWatch
+	swGetData,
+	swSortData,
+	swOutput_itoa,
+	swOutput_myitoa,
+	swOutput_mySIMDitoa;
 
-void getData(void);
-void sortDataOmpFor(void);
-void testData(void);
-void output2StringsOmpFor_itoa(void);
-void output2StringsOmpFor_myitoa(void);
-void outputTimes(void);
+void getData();
+void sortDataOmpFor();
+void testData();
+void output2StringsOmpFor_itoa();
+void output2StringsOmpFor_myitoa();
+void output2StringsOmpFor_mySIMDitoa();
+void outputTimes();
 
 
 int main(void)
 {
-	s3.startTimer();
+	swGetData.startTimer();
 		getData();
-	s3.stopTimer();
+	swGetData.stopTimer();
 
-	s1.startTimer();
+	swSortData.startTimer();
 		sortDataOmpFor();
-	s1.stopTimer();
+	swSortData.stopTimer();
 
 		testData();
 
-	s2.startTimer();
-		//output2StringsOmpFor_itoa();
+	swOutput_itoa.startTimer();
+		output2StringsOmpFor_itoa();
+	swOutput_itoa.stopTimer();
+
+	swOutput_myitoa.startTimer();
 		output2StringsOmpFor_myitoa();
-	s2.stopTimer();
+	swOutput_myitoa.stopTimer();
+
+	swOutput_mySIMDitoa.startTimer();
+		output2StringsOmpFor_mySIMDitoa();
+	swOutput_mySIMDitoa.stopTimer();
 
 		outputTimes();
 
@@ -138,7 +151,7 @@ void output2StringsOmpFor_itoa()
 {
 	char numString[MAX_CHARS];
 	string odataf1, odataf2;
-	cout << "\n\noutputting data to sodata.txt...";
+	cout << "\n\noutputting data to sodata_itoa.txt...";
 
 	//#pragma omp parallel for private (numString) schedule (static, MAX_ROWS/2)
 	for(int i=0; i<MAX_ROWS; i++){
@@ -160,7 +173,7 @@ void output2StringsOmpFor_itoa()
 	}
 
 	FILE * sodata;
-	sodata = fopen ("sodata.txt","w");
+	sodata = fopen ("sodata_itoa.txt","w");
 	fputs(odataf1.c_str(), sodata);
 	fputs(odataf2.c_str(), sodata);
 	fclose (sodata);
@@ -173,7 +186,7 @@ void output2StringsOmpFor_myitoa()
 
 	char numString[MAX_CHARS];
 	string odataf1, odataf2;
-	cout << "\n\noutputting data to sodata.txt...";
+	cout << "\n\noutputting data to sodata_myitoa.txt...";
 
 	//#pragma omp parallel for private (numString) schedule (static, MAX_ROWS/2)
 	for(int i=0; i<MAX_ROWS; i++){
@@ -195,12 +208,46 @@ void output2StringsOmpFor_myitoa()
 	}
 
 	FILE * sodata;
-	sodata = fopen ("sodata.txt","w");
+	sodata = fopen ("sodata_myitoa.txt","w");
 	fputs(odataf1.c_str(), sodata);
 	fputs(odataf2.c_str(), sodata);
 	fclose (sodata);
 }
 
+//builds two half-file strings in parallel using custom fn
+void output2StringsOmpFor_mySIMDitoa()
+{
+	void mySIMDitoa(int num, char * numstr);
+
+	char numString[MAX_CHARS];
+	string odataf1, odataf2;
+	cout << "\n\noutputting data to sodata_mySIMDitoa.txt...";
+
+	//#pragma omp parallel for private (numString) schedule (static, MAX_ROWS/2)
+	for(int i=0; i<MAX_ROWS; i++){
+		for(int j=0; j<MAX_COLS; j++){
+			mySIMDitoa(data[i][j],numString);
+			if(i < MAX_ROWS/2) {
+				odataf1 += numString;
+				odataf1+="\t";
+			}
+			else {
+				odataf2 += numString;
+				odataf2+="\t";
+			}
+		}
+		if(i < MAX_ROWS/2)
+			odataf1+="\n";
+		else
+			odataf2+="\n";
+	}
+
+	FILE * sodata;
+	sodata = fopen ("sodata_mySIMDitoa.txt","w");
+	fputs(odataf1.c_str(), sodata);
+	fputs(odataf2.c_str(), sodata);
+	fclose (sodata);
+}
 
 /*****  myitoa fast integer to ascii fn *****
  *****  copyright Simon Andrews, 2012   *****/
@@ -259,10 +306,71 @@ enditoa:
 	}
 }
 
+
+void mySIMDitoa(int num, char * numstr)
+{
+	__asm {
+			mov		ebx,numstr		; point ebx to numstr
+			mov		esi, num		; store number in esi
+			cmp		esi, 0			; if number is 0
+			jne		nextdigit
+			mov     [ebx],48		; just set numstr to "0"
+			mov     [ebx+1],0		; add terminatting null character
+			jmp		enditoa			; and end
+
+nextdigit:	mov		eax, 1999999Ah		;1999999Ah = 2^34 / (10 * 2^2) (magic number!)
+			imul	esi					;IMUL ESI >> EDX:EAX <-- EAX * ESI
+										;Therefore, edx:eax = number * 2^34 / (10 * 2^2)
+										;Therefore edx	= number * 2^34 / (10 * 2^2 * 2^32)
+										;				= number * 2^34 /(10 * 2^34)
+										;				= number / 10 (integer division)
+
+			lea		ecx, [edx + edx*4]	; ecx = edx * 5
+			add		ecx,ecx				; ecx = edx * 10
+										; therfore ecx = (number div 10) * 10
+
+			mov		eax,esi				; store original number in eax
+			sub		eax,ecx				; subtract ecx to leave remainder in eax
+
+			add		eax,48				; add 48 to make eax the digit's ASCII code
+
+			mov		[ebx],al			; store digit character in numstr
+			inc		ebx
+
+			mov		esi,edx				; move number div 10 back into esi
+			cmp		esi,0				; if number div 10 = 0, we've finished
+			jnz		nextdigit
+
+			mov     [ebx],0				; so add terminating null character
+
+; The number is in reverse order of digits
+; so we need to reverse the string.
+; Note that ebx is currently pointing to the terminating null char of the number string
+
+			mov		edx,numstr	;point edx to start
+nextChar:	dec		ebx			;move end pointer back one char		
+			cmp		ebx,edx		;compare the pointers
+			jle		enditoa		;if they haven't met or passed in the middle
+			mov		eax,[edx]	;swap the chars
+			mov		ecx,[ebx]
+			mov		[ebx],al
+			mov		[edx],cl
+			inc		edx			;move start pointer forward one char
+			jmp     nextChar
+enditoa:
+	}
+}
+
 void outputTimes()
 {
-	cout << "\n\nTime for getting: " << s3.getElapsedTime();
-	cout << "\n\nTime for sorting: " << s1.getElapsedTime();
-	cout << "\nTime for output : " << s2.getElapsedTime();
-	cout << "\nCombined time   : " << s1.getElapsedTime() + s2.getElapsedTime() + s3.getElapsedTime();
+	cout << "\n\nTime for getting: " << swGetData.getElapsedTime();
+	cout << "\n\nTime for sorting: " << swSortData.getElapsedTime();
+	cout << "\nTime for output itoa: " << swOutput_itoa.getElapsedTime();
+	cout << "\nTime for output myitoa: " << swOutput_myitoa.getElapsedTime();
+	cout << "\nTime for output mySIMDitoa: " << swOutput_mySIMDitoa.getElapsedTime();
+
+	float difference = swOutput_myitoa.getElapsedTime() - swOutput_mySIMDitoa.getElapsedTime();
+
+	cout << "\n\nPercentage: " << (difference/swOutput_myitoa.getElapsedTime())*100 << "%";
+	
 }
